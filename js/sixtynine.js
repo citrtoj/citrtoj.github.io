3e24826ccb720f7d9f34b86b83de8ad5c615e6fd
citrtoj <citrtoj@gmail.com>
2021-07-29 15:23:11 +0300

    Switched API for sixtynine

--- js/sixtynine.js
+++ js/sixtynine.js
@@ -6,7 +6,6 @@ function fToC(fahrenheit)
 } 
 document.getElementById("searchUser").focus();
 document.getElementById("searchUser").value="";
-const key="pk.c32bb53e9d7fad7298e5c4c9bb93b48a"
 
 function truncate(input) {
    if (input.length > 46) {
@@ -23,7 +22,7 @@ function submitt(){
 	
 	document.getElementById("results").classList.add("gone");
 	var revvalue=document.getElementById("searchUser").value;
-	fetch(`https://api.locationiq.com/v1/autocomplete.php?key=${key}&limit=5&q="${revvalue}"&tag=place:city,place:town,place:borough,place:village,place:island,place:islet&dedupe=1`).then(function(response3){
+	fetch(`https://photon.komoot.io/api/?limit=5&q=${revvalue}&osm_tag=place:city&osm_tag=place:town&osm_tag=place:borough&osm_tag=place:village&osm_tag=place:hamlet&osm_tag=place:island&osm_tag=place:islet`).then(function(response3){
 		if(response3.ok){
 			return response3.json();
 		}else{
@@ -31,10 +30,10 @@ function submitt(){
 		}
 	}).then(function(data3){
 		var datar=JSON.parse(JSON.stringify(data3));
-		
-		var lat3=datar[0].lat;
-		var lon3=datar[0].lon;
-		
+
+		var lat3=datar.features[0].geometry.coordinates[1];
+		var lon3=datar.features[0].geometry.coordinates[0];
+		var dats=datar.features[0];
 		fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat3}&units=imperial&lon=${lon3}&appid=0ad8fc43e121ce281e27f86cd106ce96`).then(function(response4){
 					if(response4.ok){
 						return response4.json();
@@ -44,23 +43,23 @@ function submitt(){
 				}).then(function(data4){
 					var dataw4=JSON.parse(JSON.stringify(data4));
 					var temp=dataw4.main.temp;
+					var licontent=dats.properties.name + ", "  + (dats.properties.hasOwnProperty('county')?dats.properties.county+ ", ":"")+ (dats.properties.hasOwnProperty('state')?dats.properties.state+ ", ":"") + (dats.properties.hasOwnProperty('postcode')?dats.properties.postcode+ ", ":"") + dats.properties.country;
+
 					document.getElementById("answer").classList.remove("gone");
 					if(Math.round(temp)===69)
 					{
 						document.getElementById("yesno").innerHTML="Yes.";
+						document.getElementById("yesno").classList.add("green");
 						if(temp<69) document.getElementById("yesno").innerHTML+=" Pretty much."
-						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw4.main.temp + "°F / "+ (+fToC(dataw4.main.temp).toFixed(2)) +"°C</span> in " + datar[0].display_name + ".";
+						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw4.main.temp + "°F / "+ (+fToC(dataw4.main.temp).toFixed(2)) +"°C</span> in " + licontent + ".";
 						
 						document.getElementById("tryagain").innerHTML="But you can always check for other cities too!";
 					}
 					else
 					{
 						document.getElementById("yesno").innerHTML="No.";
-						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw4.main.temp + "°F / "+ (+fToC(dataw4.main.temp).toFixed(2)) +"°C</span> in " + datar[0].display_name + ".";
-						if(Math.round(fToC(temp))==42)
-						{
-							document.getElementById("fortytwo").innerHTML="...which means it's <span class='black'>42°C!</span>";
-						}
+						document.getElementById("yesno").classList.remove("green");
+						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw4.main.temp + "°F / "+ (+fToC(dataw4.main.temp).toFixed(2)) +"°C</span> in " + licontent + ".";
 						document.getElementById("tryagain").innerHTML="Try again with another city!";
 					}
 					
@@ -77,7 +76,7 @@ document.getElementById("searchUser").addEventListener("keyup",function()
 	else{
 	document.getElementById("results").classList.remove("gone");
 	var post;
-	fetch(`https://api.locationiq.com/v1/autocomplete.php?key=${key}&limit=5&q="${input}"&tag=place:city,place:town,place:borough,place:village,place:island,place:islet&dedupe=1`).then(function(response){
+	fetch(`https://photon.komoot.io/api/?limit=5&q=${input}&osm_tag=place:city&osm_tag=place:town&osm_tag=place:borough&osm_tag=place:village&osm_tag=place:hamlet&osm_tag=place:island&osm_tag=place:islet`).then(function(response){
 		if(response.ok) {
 			return response.json();
 		} else {
@@ -88,13 +87,13 @@ document.getElementById("searchUser").addEventListener("keyup",function()
 		var html="";
 		var searchbox = document.getElementById("searchUser");
 		var list=document.getElementById("results");
-		var datap=JSON.parse(JSON.stringify(data));
+		var datap=JSON.parse(JSON.stringify(data)).features;
 		document.getElementById("results").innerHTML="";
 		datap.forEach(function(dat){
 			var newel=document.createElement("li");
 			newel.classList.add("autores");
 			
-			var licontent=dat.display_name;
+			var licontent=dat.properties.name + ", "  + (dat.properties.hasOwnProperty('county')?dat.properties.county+ ", ":"")+ (dat.properties.hasOwnProperty('state')?dat.properties.state+ ", ":"") + (dat.properties.hasOwnProperty('postcode')?dat.properties.postcode+ ", ":"") + dat.properties.country;
 			newel.innerHTML=licontent;
 			document.getElementById("results").appendChild(newel);
 			newel.addEventListener("click",function(){
@@ -105,9 +104,9 @@ document.getElementById("searchUser").addEventListener("keyup",function()
 				document.getElementById("yesno").innerHTML="";
 				document.getElementById("tryagain").innerHTML="";
 				document.getElementById("fortytwo").innerHTML="";
-				searchbox.value=dat.display_name;
-				var lat=dat.lat;
-				var lon=dat.lon;
+				searchbox.value=licontent;
+				var lat=dat.geometry.coordinates[1];
+				var lon=dat.geometry.coordinates[0];
 				fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=imperial&lon=${lon}&appid=0ad8fc43e121ce281e27f86cd106ce96`).then(function(response2){
 					if(response2.ok){
 						return response2.json();
@@ -121,14 +120,17 @@ document.getElementById("searchUser").addEventListener("keyup",function()
 					if(Math.round(temp)===69)
 					{
 						document.getElementById("yesno").innerHTML="Yes."
+						document.getElementById("yesno").classList.add("green");
 						if(temp<69) document.getElementById("yesno").innerHTML+=" Pretty much."
-						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw.main.temp + "°F / "+(+fToC(dataw.main.temp).toFixed(2)) +"°C</span> in " + dat.display_name + ".";
+						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw.main.temp + "°F / "+(+fToC(dataw.main.temp).toFixed(2)) +"°C</span> in " + licontent + ".";
 						document.getElementById("tryagain").innerHTML="But you can always check for other cities too!"
 					}
 					else
 					{
 						document.getElementById("yesno").innerHTML="No."
-						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw.main.temp + "°F / "+(+fToC(dataw.main.temp).toFixed(2)) +"°C</span> in " + dat.display_name + ".";
+						document.getElementById("yesno").classList.remove("green");
+						document.getElementById("expl").innerHTML="It is <span class='black'>" + dataw.main.temp + "°F / "+(+fToC(dataw.main.temp).toFixed(2)) +"°C</span> in " + licontent + ".";
+						
 						document.getElementById("tryagain").innerHTML="Try again with another city!"
 					}
 				}).catch(function(error){
